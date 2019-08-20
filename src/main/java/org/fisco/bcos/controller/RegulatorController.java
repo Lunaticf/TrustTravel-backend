962b13a12f75c0d4f33c3fde7ceee4af5be6ce9b
Lunaticf <724922441@qq.com>
2019-08-20 20:19:51 +0800

    add task

---
+++ src/main/java/org/fisco/bcos/controller/RegulatorController.java
@@ -0,0 +1,18 @@
+package org.fisco.bcos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 监管节点 控制层
+ */
+@Controller
+public class RegulatorController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
